e196be1a8b708db2cb7771414fa903a466524aab
Alexi Carey <ciegoproductions@gmail.com>
2020-09-27 15:21:29 -0600

    Add um_order_comment to order object, add category attributes, bump version to 0.1.2

--- lib/model/category.rb
+++ lib/model/category.rb
@@ -1,6 +1,8 @@
 module M2api
   class Category < Base
-    #TODO implement
+    Attributes = %i(id parent_id name is_active position level product_count children_data)
+    Attributes.each do |attr| attr_accessor attr end
+
     class << self
       def base_path
         "categories"
